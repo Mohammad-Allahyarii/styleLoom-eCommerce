97ebe53712c404d65d3485ef50fe4bfad1459639
REstesna <mohammad.allahyari.am@gmail.com>
2026-07-01 12:44:21 +0330

    [ADD] container component

--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -1,11 +1,29 @@
-import type { ElementType } from 'react';
+import type React from 'react';
+import type { ElementType, HTMLAttributes } from 'react';
+
+import type { ClassValue } from 'clsx';
+import clsx from 'clsx';
+
+// type PropsType<E extends ElementType = 'div'> = {
+//   as: E;
+//   className?: ClassValue;
+//   children?: React.ReactNode;
+// } & Omit<HTMLAttributes<HTMLElement>, 'as'>;
 
 type PropsType = {
   as: ElementType;
-};
+  className?: ClassValue;
+  children?: React.ReactNode;
+} & Omit<HTMLAttributes<HTMLElement>, 'as'>;
 
-const Container = ({ as: Element = 'div' }: PropsType) => {
-  return <Element>Container</Element>;
+const Container = ({ as: Element = 'div', className, children }: PropsType) => {
+  return (
+    <Element
+      className={clsx('container lg:max-w-7xl mx-auto', className)}
+    >
+      {children}
+    </Element>
+  );
 };
 
 export default Container;
