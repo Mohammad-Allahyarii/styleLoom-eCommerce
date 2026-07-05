4a2eb4d73e9ac3ecfca5fdf03572d84bd8a3a5bd
REstesna <mohammad.allahyari.am@gmail.com>
2026-07-05 13:08:31 +0330

    [RESPNSIVE] add hamberger menu button for rsponsive menu

--- src/Layouts/MainAppLayou/components/LayoutNavbar.tsx
+++ src/Layouts/MainAppLayou/components/LayoutNavbar.tsx
@@ -1,4 +1,4 @@
-import { ShoppingCart } from 'lucide-react';
+import { Menu, ShoppingCart } from 'lucide-react';
 
 import logo from '@/assets/logos/Logo.svg';
 import Button from '@/components/button/Button';
@@ -7,24 +7,28 @@ import DashedLine from '@/components/dashedLine/DashedLine';
 
 const LayoutNavbar = () => {
   return (
-    <Container as="nav" className="flex items-center justify-between relative">
+    <Container as="nav" className="flex items-center justify-between relative py-6">
       {/* left */}
-      <div className="py-6 flex items-center gap-3">
+      <div className=" hidden md:flex items-center gap-3">
         <Button variant="secondary">Home</Button>
         <Button variant="bordered">Products</Button>
       </div>
 
       {/* center */}
-      <div>
+      <div className=''>
         <img src={logo} alt="Logo" />
       </div>
 
       {/* right */}
-      <div className="flex justify-end items-center gap-3">
+      <div className="hidden md:flex justify-end items-center gap-3">
         <Button variant="secondary" icon={ShoppingCart} />
 
         <Button>Contact</Button>
       </div>
+
+      <Button className="aspect-square p-4! md:hidden">
+        <Menu />
+      </Button>
 
       <DashedLine />
     </Container>
