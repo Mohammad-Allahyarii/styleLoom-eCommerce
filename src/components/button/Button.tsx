69ae4bb002c3546e3ae217d9cbd35ffa2d5efd5f
REstesna <mohammad.allahyari.am@gmail.com>
2026-07-18 22:46:47 +0330

    refactor: change merge class way

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -12,7 +12,7 @@ type PropsType = {
   onClick?: () => void;
 };
 
-const dashedBorderImage = `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='12' ry='12' stroke='%23262626FF' stroke-width='3' stroke-dasharray='3%2c 10' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e")`
+const dashedBorderImage = `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='12' ry='12' stroke='%23262626FF' stroke-width='3' stroke-dasharray='3%2c 10' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e")`;
 
 const Button = ({
   children,
@@ -29,13 +29,14 @@ const Button = ({
     <motion.button
       className={clsx(
         buttonClass,
-        className,
-        'max-w-max rounded-xl relative py-2.5 px-5 cursor-pointer transition-all duration-300 ease-in-out text-[16px] lg:text-[16px]',
+        `max-w-max rounded-xl relative py-2.5 px-5 cursor-pointer transition-all duration-300 ease-in-out text-[16px] lg:text-[16px] ${className}`,
       )}
-      whileHover={{ scale: 1.02 }}
       onClick={onClick}
       style={{
-        backgroundImage:  variant === 'cornerBordered' || variant === 'bordered' ? dashedBorderImage : 'none'
+        backgroundImage:
+          variant === 'cornerBordered' || variant === 'bordered'
+            ? dashedBorderImage
+            : 'none',
       }}
     >
       {children}
