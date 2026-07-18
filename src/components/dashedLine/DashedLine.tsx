fbb5adc95c57f8c2fa0005993264fe4e991bc191
REstesna <mohammad.allahyari.am@gmail.com>
2026-07-18 22:47:51 +0330

    refactor: add axis to component and add class ability

--- src/components/dashedLine/DashedLine.tsx
+++ src/components/dashedLine/DashedLine.tsx
@@ -1,14 +1,26 @@
+import clsx from 'clsx';
 
+type DashedLineProps = {
+  axis?: 'horizontal' | 'vertical';
+  className?: string;
+};
 
-const DashedLine = () => {
+const DashedLine = ({ axis = 'horizontal', className }: DashedLineProps) => {
+  const isHorizontal = axis === 'horizontal';
+
   return (
-    <svg className={"w-full"} height="1" preserveAspectRatio="none">
+    <svg
+      className={clsx(isHorizontal ? 'w-full' : 'h-full', className)}
+      width={isHorizontal ? '100%' : '1'}
+      height={isHorizontal ? '1' : '100%'}
+      preserveAspectRatio="none"
+    >
       <line
         className="stroke-dark-15"
         x1="0"
         y1="0"
-        x2="100%"
-        y2="0"
+        x2={isHorizontal ? '100%' : '0'}
+        y2={isHorizontal ? '0' : '100%'}
         stroke="black"
         strokeWidth="3"
         strokeDasharray="10 8"
