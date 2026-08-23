299cfdea6447eb43048288b168aa8ffa1020614f
REstesna <mohammad.allahyari.am@gmail.com>
2026-08-24 00:54:02 +0330

    add: sections to homePage

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,10 +1,15 @@
 import Container from "@/components/container/Container";
+import CraftingTrendsSection from "@/Pages/HomePage/components/CraftingTrendsSection";
+import HeroSction from "@/Pages/HomePage/components/HeroSction";
+import NavigatingStyleloom from "@/Pages/HomePage/components/NavigatingStyleloom";
 
 
 const HomePage = () => {
   return (
-    <Container as="div">
-      {/* <HeroSection /> */}
+    <Container as="div" className={'flex flex-col gap-10 pb-10'}>
+      <HeroSction />
+      <CraftingTrendsSection />
+      <NavigatingStyleloom />
       
     </Container>
   );
