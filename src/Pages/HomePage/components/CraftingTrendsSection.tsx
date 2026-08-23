0f7e9116283ccbd9d81acc199173c15fed6a5ce6
REstesna <mohammad.allahyari.am@gmail.com>
2026-08-24 00:53:37 +0330

    add: Crafting section to homePage

---
+++ src/Pages/HomePage/components/CraftingTrendsSection.tsx
@@ -0,0 +1,23 @@
+import HomeSectionTemplate from '@/Pages/HomePage/components/HomeSectionTemplate';
+import TrendCraftCard from '@/Pages/HomePage/components/TrendCraftCard';
+import { CRAFTING_TREND_SECTION } from '@/constants/constants';
+
+const CraftingTrendsSection = () => {
+  // Map through the CRAFTING_TREND_SECTION array and create TrendCraftCard components for each item
+  const craftItemsElement = CRAFTING_TREND_SECTION.map((item) => (
+    <TrendCraftCard key={item.id} item={item} />
+  ));
+
+  return (
+    <HomeSectionTemplate
+      itemsElement={craftItemsElement}
+      headerInfo={{
+        title: 'Crafting Trends, Inspiring Confidence',
+        description:
+          'Explore a world of fashion at StyleLoom, where trends meet affordability.',
+      }}
+    />
+  );
+};
+
+export default CraftingTrendsSection;
---
+++ src/Pages/HomePage/components/TrendCraftCard.tsx
@@ -0,0 +1,27 @@
+import type { CRAFTING_TREND_SECTION_type } from "@/constants/constants";
+
+
+
+const TrendCraftCard = ({item}: {item: CRAFTING_TREND_SECTION_type}) => {
+  return (
+    <div className="relative px-7.5 py-7.5 flex flex-col justify-start items-start gap-6 ">
+      {/* absolute image  */}
+      <img
+        className="absolute right-0 top-0"
+        src={item.sideIcon}
+        alt={item.sideIcon}
+      />
+
+      {/* main icon */}
+      <img src={item.mainIcon} alt={item.title} />
+      <div>
+        <h6 className="font-roboto-medium text-lg text-absolute-white mb-2.5">
+          {item.title}
+        </h6>
+        <p className="font-roboto-regular text-sm text-grey-50">{item.desc}</p>
+      </div>
+    </div>
+  );
+};
+
+export default TrendCraftCard;
