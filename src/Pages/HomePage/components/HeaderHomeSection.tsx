1fd064499ed13bd6460cea89ef37bc784738b6fa
REstesna <mohammad.allahyari.am@gmail.com>
2026-09-06 15:13:42 +0330

    refactor: hide header image in mobile

--- src/Pages/HomePage/components/HeaderHomeSection.tsx
+++ src/Pages/HomePage/components/HeaderHomeSection.tsx
@@ -1,9 +1,10 @@
+import useMediaQuery from '@/hooks/useMediaQuery';
 import { type ReactNode } from 'react';
 
 interface PropsType {
   title: ReactNode;
   description?: ReactNode;
-  otherNodes?: ReactNode[];
+  otherNodes?: ReactNode;
   imgAdress?: string;
 }
 
@@ -13,18 +14,21 @@ const HeaderHomeSection = ({
   otherNodes,
   imgAdress,
 }: PropsType) => {
+
+  const isMobile = useMediaQuery("(max-width: 420px)");
+
   return (
-    <div className="py-10 md:py-15 px-4 md:px-12 flex flex-col gap-2 relative overflow-hidden">
+    <div className="py-10 md:py-15 px-4 md:px-12 flex flex-col gap-2 relative overflow-hidden rounded-tr-2xl">
         <h4 className="text-[28px] text-absolute-white font-roboto-medium uppercase">
           {title}
         </h4>
         <p className="font-roboto-regular text-grey-40 text-[14px]">
           {description}
         </p>
-        {otherNodes?.map((item) => item)}
+        {otherNodes}
 
-      {imgAdress && (
-        <div className='max-w-80 absolute -right-20 -top-10'>
+      {imgAdress && !isMobile && (
+        <div className='md:max-w-80 absolute -right-20 -top-10'>
           <img src={imgAdress} alt="header image" className="w-full h-auto" />
         </div>
       )}
