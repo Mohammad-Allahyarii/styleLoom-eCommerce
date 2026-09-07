630a6171a146e0518ee7a06fcbbc6aec00481bb2
REstesna <mohammad.allahyari.am@gmail.com>
2026-09-07 22:54:06 +0330

    refactor: refactor filterButtons to a reusable component

---
+++ src/components/FilterButtons/FilterButtons.tsx
@@ -0,0 +1,35 @@
+import Button from "@/components/button/Button";
+
+interface FilterButtonsProps<T extends string> {
+  value: T;
+  onChange: React.Dispatch<React.SetStateAction<T>>;
+  options: T[];
+  getLabel?: (option: T) => string;
+}
+
+function FilterButtons<T extends string>({
+  value,
+  onChange,
+  options,
+  getLabel,
+}: FilterButtonsProps<T>) {
+
+  console.log(options);
+  
+  return (
+    <div className="flex items-center justify-start flex-nowrap overflow-x-auto gap-3.5">
+      {options.map((option) => (
+        <Button
+          key={option}
+          onClick={() => onChange(option)}
+          variant={value === option ? 'primary' : 'bordered'}
+          className="font-roboto-regular! shrink-0 capitalize"
+        >
+          {getLabel ? getLabel(option) : option}
+        </Button>
+      ))}
+    </div>
+  );
+}
+
+export default FilterButtons;
