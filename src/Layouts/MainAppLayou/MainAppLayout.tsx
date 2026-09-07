94d8479154bab7c910e94e04215275ce6039e186
REstesna <mohammad.allahyari.am@gmail.com>
2026-09-07 22:57:02 +0330

    refactor: add main components to main App Layout

--- src/Layouts/MainAppLayou/MainAppLayout.tsx
+++ src/Layouts/MainAppLayou/MainAppLayout.tsx
@@ -1,15 +1,21 @@
 import { Outlet } from 'react-router';
 
-import Container from '@/components/container/Container';
-import LayoutNavbar from '@/Layouts/MainAppLayou/components/LayoutNavbar';
 import LayoutFooter from '@/Layouts/MainAppLayou/components/LayoutFooter';
+import LayoutNavbar from '@/Layouts/MainAppLayou/components/LayoutNavbar';
+import CTABannerSection from '@/Pages/HomePage/components/CTABannerSection/CTABannerSection';
+import FaqSection from '@/Pages/HomePage/components/FaqSection/FaqSection';
+import UserReviewsSection from '@/Pages/HomePage/components/UserReviewsSection/UserReviewsSection';
+import Container from '@/components/container/Container';
 
 const MainAppLayout = () => {
   return (
     <>
       <LayoutNavbar />
-      <Container as="main" className="flex-1">
+      <Container as="main" className="flex-1 flex flex-col gap-10 pb-10 pt-10">
         <Outlet />
+        <UserReviewsSection />
+        <FaqSection />
+        <CTABannerSection />
       </Container>
       <LayoutFooter />
     </>
