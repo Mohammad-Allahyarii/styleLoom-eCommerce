0429db09e0ca6a30b5ecb341bdf61365d9dcd0ac
REstesna <mohammad.allahyari.am@gmail.com>
2026-09-08 14:54:08 +0330

    refactor: remove extra imports

--- src/Pages/HomePage/components/ElevateStyleSection.tsx
+++ src/Pages/HomePage/components/ElevateStyleSection.tsx
@@ -1,12 +1,8 @@
 import { useEffect, useState } from 'react';
 
-import { MoveUpRight } from 'lucide-react';
-
 import HEADER_IMAGE from '@/assets/images/elevate-section/Abstract Design.svg';
 import MainSectionTemplate from '@/components/MainSectionTelmplate/MainSectionTemplate';
 import ProductCard from '@/components/ProductCard/ProductCard';
-import Button from '@/components/button/Button';
-import DashedBox from '@/components/dashedBox/DashedBox';
 import { PRODUCTS, type PRODUCT_type } from '@/constants/constants';
 
 type currentSelectedCategoryType = 'all' | PRODUCT_type['category'];
